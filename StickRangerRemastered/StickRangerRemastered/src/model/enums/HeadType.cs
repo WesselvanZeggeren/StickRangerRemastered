9ecf3e962d94e918a08e15f0b5f75f88f17e6c70
Wessel <wesselvanz@gmail.com>
2019-09-11 11:47:18 +0200

    Small changes

---
+++ StickRangerRemastered/StickRangerRemastered/src/model/enums/HeadType.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickRangerRemasterd.src.model.enums
+{
+
+    enum HeadType
+    {
+
+        smileyNeurtal       = 0x00,
+        smileyHappy         = 0x01,
+        x                   = 0x02,
+        skull               = 0x03,
+        fairy               = 0x04,
+        cap                 = 0x05,
+        roundHeadNeurtal    = 0x06,
+        roundHeadInverted   = 0x07,
+        gel                 = 0x08,
+        star                = 0x09,
+        mask                = 0x10,
+        boxNormal           = 0x11,
+        boxSleepy           = 0x12,
+        diamond             = 0x13,
+        vampire             = 0x14,
+        shield              = 0x15,
+        castle              = 0x16,
+        demon               = 0x17,
+        coconut             = 0x18,
+        triangle            = 0x19
+    }
+}
